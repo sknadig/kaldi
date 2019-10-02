6ccbee73ead3a8d2fc675d9be7689223a131937d
sknadig <shreekanthnadig@gmail.com>
2019-10-02 23:33:16 +0530

    create alignment pkl dict

--- egs/timit/s5/generate_alignments.sh
+++ egs/timit/s5/generate_alignments.sh
@@ -6,6 +6,7 @@ set -e
 
 for x in train test dev; do
     for alignment in exp/mono_ali_"$x"/ali.*.gz; do
+        echo "aligning data in $alignment"
         ali-to-phones --per-frame exp/mono/final.mdl "ark:gunzip -c $alignment |" ark,t:- | utils/int2sym.pl -f 2- data/lang/phones.txt > $alignment.txt
     done
 done
